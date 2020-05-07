215ea1710f010ca561ab80521bbe17d1cac5b110
Fay <1044913875@qq.com>
2020-05-07 15:18:53 +0800

    simplyfy

--- src/main/java/com/sql/antlr3/hive/parse/ParseDriver.java
+++ src/main/java/com/sql/antlr3/hive/parse/ParseDriver.java
@@ -26,7 +26,6 @@ import org.antlr.runtime.tree.CommonTreeAdaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.naming.InsufficientResourcesException;
 
 /**
  * ParseDriver.
